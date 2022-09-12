8c62ad5e7e823e849cb38b9df23ed4c137f87dd3
Martin Váňa <hallowedbythyname@seznam.cz>
2022-09-12 20:26:54 +0200

    backup code ???

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 import setuptools
 
+# this is only necessary when not using setuptools/distribute
+from sphinx.setup_command import BuildDoc
+cmdclass = {'build_sphinx': BuildDoc}
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+name = 'homework'
+version = '0.3'
+
 setuptools.setup(
-    name='homework',
-    version='0.2',
+    name=name,
+    version=version,
     author="Martin Váňa",
     author_email="vana1martin@gmail.com",
     description="An example pip package",
@@ -21,5 +29,14 @@ setuptools.setup(
     python_requires='>=3.6',
     install_requires=[
         'numpy',
+        'Sphinx',
     ],
+    cmdclass=cmdclass,
+    # these are optional and override conf.py settings
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version)
+        }
+    },
 )
